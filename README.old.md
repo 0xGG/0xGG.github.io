d4485cf0c6c7317b2dec298d6313fd104e0abc00
Yiyi Wang <shd101wyy@gmail.com>
2020-06-05 21:34:24 +0800

    Updates

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# 0xgg.github.io
---
+++ precache-manifest.b7dc285716d62d72d0e892ddc930b53a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544c39c64d5f286de1359a22a753104a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92153694a3224d39c2c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd502ae80dec4f7113e5",
+    "url": "/static/js/2.bdc11520.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdc11520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92153694a3224d39c2c",
+    "url": "/static/js/main.195cb906.chunk.js"
+  },
+  {
+    "revision": "c7ac54787ca91d84b4dd",
+    "url": "/static/js/runtime-main.9c2a1bad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
